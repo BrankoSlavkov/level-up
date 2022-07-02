2a52c2d7645de0db7a39eb96ad1c3b427ae639df
BrankoSlavkov <slavkov.branko@gmail.com>
2022-07-02 11:43:05 +0200

    Header added with initial state missing drop downs

--- e-commerce-us/src/components/header/Header.tsx
+++ e-commerce-us/src/components/header/Header.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import { Plus } from '../../assets/icons/Plus';
+
+import styles from './header.module.css';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header__container}>
+      <div>sort dds</div>
+      <div>
+        <button className={styles['btn__product--add']}>
+          <Plus />
+          <span>Add Product</span>
+        </button>
+      </div>
+    </header>
+  );
 };
--- e-commerce-us/src/pages/Products.tsx
+++ e-commerce-us/src/pages/Products.tsx
@@ -6,6 +6,7 @@ import { useAppDispatch } from '../store/hooks';
 import { ProductsList } from '../components/productsList/ProductsList';
 
 import styles from './product.module.css';
+import { Header } from '../components/header/Header';
 
 export const Products = () => {
   const dispatch = useAppDispatch();
@@ -16,6 +17,7 @@ export const Products = () => {
 
   return (
     <main className={styles.main__container}>
+      <Header />
       <div className={styles.products__container}>
         <ProductsList />
         <div className={styles.product__details}>
