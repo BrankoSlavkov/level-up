6b8ac9f68044203e432f72a2469fa6442532b1d0
BrankoSlavkov <slavkov.branko@gmail.com>
2022-08-30 17:22:04 +0200

    header implemented

--- eventr/src/components/navigation/Navigation.tsx
+++ eventr/src/components/navigation/Navigation.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import styles from './navigation.module.scss';
 
 export const Navigation = () => {
   return (
     <main className={styles._container}>
-      <div>Brankooo</div>
+      <header className={styles._header}>
+        <Link to="/events">All events</Link>
+        <Link to="/events/add">New event</Link>
+        <button type="button">Logout</button>
+      </header>
+
       <Outlet />
     </main>
   );
