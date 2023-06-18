bd3f9c354eaa60fd74aac30789965bb4e0577930
BrankoSlavkov <slavkov.branko@gmail.com>
2023-06-18 16:11:48 +0200

    naming

--- e-commerce-us/client/playwright.config.ts
+++ e-commerce-us/client/playwright.config.ts
@@ -24,10 +24,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
   },
 
   /* Configure projects for major browsers */
@@ -70,8 +74,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //   command: 'yarn dev',
+  //   url: 'https://localhost:5173',
+  //    reuseExistingServer: !process.env.CI,
   // },
 });
