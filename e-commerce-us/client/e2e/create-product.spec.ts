7ae6d4992e2d27ce755372b3ead43b593ed3a337
BrankoSlavkov <slavkov.branko@gmail.com>
2023-06-18 16:57:58 +0200

    styles fixes

--- e-commerce-us/client/e2e/create-product.spec.ts
+++ e-commerce-us/client/e2e/create-product.spec.ts
@@ -1,26 +1,54 @@
 import { expect, test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
-test('test', async ({ page }) => {
-  const productName = faker.commerce.productName();
+import { fetchData } from '../src/api/fetchDate';
+import { CATEGORIES_URL, STATES_URL } from '../src/constants/urls';
 
-  await page.goto('/products');
-  await page.getByRole('button', { name: 'Add Product' }).click();
-  await page.locator('select[name="stateId"]').selectOption('1u4JNjlJf9APT');
-  await page.locator('select[name="categoryId"]').selectOption('iwg36e9Vrh6j2');
-  await page.getByPlaceholder('Title').fill(productName);
-  await page.getByPlaceholder('Price').fill(faker.commerce.price({ min: 4 }));
-  await page
-    .getByPlaceholder('Image url')
-    .fill(faker.image.urlLoremFlickr({ category: 'Furniture' }));
-  await page
-    .getByPlaceholder('Description')
-    .fill(faker.commerce.productDescription());
-  await page.getByRole('button', { name: /Submit/i }).click();
+import { Category } from '../src/store/category/categoryTypes';
+import { State } from '../src/store/state/stateTypes';
 
-  expect(
-    page.getByRole('link', {
-      name: new RegExp(`${productName}`, 'gi'),
-    }),
-  ).not.toBeNull();
+test.describe('Create Product', () => {
+  let categories: Category[] = [];
+  let states: State[] = [];
+
+  test.beforeAll(async () => {
+    categories = await fetchData(CATEGORIES_URL);
+    states = await fetchData(STATES_URL);
+  });
+
+  // test.afterAll(async ({ page }) => {
+  //   await page.waitForTimeout(1000);
+  //   await page.close();
+  // });
+
+  test('test', async ({ page }) => {
+    const productName = faker.commerce.productName();
+    const randomCategory =
+      categories[Math.floor(Math.random() * categories.length)];
+    const randomState = states[Math.floor(Math.random() * states.length)];
+
+    await page.goto('/products');
+    await page.getByRole('button', { name: 'Add Product' }).click();
+    await page.locator('select[name="stateId"]').selectOption(randomState.id);
+    await page
+      .locator('select[name="categoryId"]')
+      .selectOption(randomCategory.id);
+    await page.getByPlaceholder('Title').fill(productName);
+    await page.getByPlaceholder('Price').fill(faker.commerce.price({ min: 4 }));
+    await page.getByPlaceholder('Image url').fill(
+      faker.image.urlLoremFlickr({
+        category: randomCategory.name,
+      }),
+    );
+    await page
+      .getByPlaceholder('Description')
+      .fill(faker.commerce.productDescription());
+    await page.getByRole('button', { name: /Submit/i }).click();
+
+    expect(
+      page.getByRole('link', {
+        name: new RegExp(`${productName}`, 'gi'),
+      }),
+    ).not.toBeNull();
+  });
 });
